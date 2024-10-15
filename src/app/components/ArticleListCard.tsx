c7b82857304e4e3830f9a3c658e85d0b7e4d0a3e
k65miyazakiy <k65miyazakiy@gmail.com>
2024-10-15 22:56:04 +0900

    タグコンポーネントの改善と記事の一覧カードのタグリストの修正。

--- src/app/components/ArticleListCard.tsx
+++ src/app/components/ArticleListCard.tsx
@@ -1,12 +1,15 @@
 import { ArticleMeta } from "../lib/util";
+import { Tags } from "./Tags";
 
 export const ArticleListCard = (props: ArticleMeta) => {
   return (
-    <article className="w-[768px] rounded-lg border p-4">
+    <article className="w-[768px] rounded-lg border px-4 pt-4">
       <h1 className="mb-2 text-xl">{props.title}</h1>
       <div className="mb-4 flex items-center gap-3">
         <span className="text-sm text-gray-600">{props.createdAt}</span>
-        <span className="text-sm text-gray-600">{props.tags}</span>
+        <span className="text-sm text-gray-600">
+          <Tags tags={props.tags} />
+        </span>
       </div>
     </article>
   );
