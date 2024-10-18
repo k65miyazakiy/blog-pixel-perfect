935d5e9bc86e10c59fe19c6a1b52e87b40075601
k65miyazakiy <k65miyazakiy@gmail.com>
2024-10-18 20:25:16 +0900

    スタイル調整

--- src/app/components/Tags.tsx
+++ src/app/components/Tags.tsx
@@ -6,7 +6,7 @@ export const Tags = ({ tags }: { tags?: string[] }) => {
       {tags.map((tag) => (
         <li
           key={tag}
-          className="rounded-2xl border border-gray-400 px-2 py-0.5 font-mono text-xs text-gray-400 transition-colors duration-300 ease-in before:pr-1 before:content-['#'] hover:border-sky-700 hover:bg-sky-50 hover:text-sky-700"
+          className="rounded-2xl border border-gray-400 px-2 py-0.5 font-mono text-xs text-gray-500 transition-colors duration-300 ease-in before:pr-1 before:content-['#'] hover:border-sky-700 hover:bg-sky-50 hover:text-sky-700"
         >
           <Link href={`/tags/${tag}`}>{tag}</Link>
         </li>
