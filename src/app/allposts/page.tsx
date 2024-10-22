5d5a7fdd0a8646290c42d7d06b503cab6b1a6dda
k65miyazakiy <k65miyazakiy@gmail.com>
2024-10-22 21:56:47 +0900

    変数名を改善

--- src/app/allposts/page.tsx
+++ src/app/allposts/page.tsx
@@ -3,8 +3,8 @@ import { ArticleListCard } from "../components/ArticleListCard";
 import { getLatestPostsMeta } from "../lib/util";
 
 export default function AllPosts() {
-  const latestPosts = getLatestPostsMeta(-1);
-  const postCount = latestPosts.length;
+  const allPosts = getLatestPostsMeta(-1);
+  const postCount = allPosts.length;
   return (
     <div className="flex flex-col items-center">
       <div className="mt-4 flex items-end">
@@ -12,7 +12,7 @@ export default function AllPosts() {
         <div className="pb-[5px] text-xs">（{postCount}）</div>
       </div>
       <ul className="my-4 flex w-[768px] flex-col items-center gap-4">
-        {latestPosts.map((post) => (
+        {allPosts.map((post) => (
           <li key={post.slug}>
             <Link href={`/posts${post.slug}`}>
               <ArticleListCard {...post.frontMatter} />
