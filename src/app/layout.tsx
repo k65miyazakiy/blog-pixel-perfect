46132e1d294c0fdfc2dda71c563a11954c8b896a
k65miyazakiy <k65miyazakiy@gmail.com>
2025-04-29 23:01:36 +0900

    faviconを設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from "next";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import "./globals.css";
@@ -6,6 +7,14 @@ import "./lib/fontawesome";
 import { Noto_Sans } from "next/font/google";
 const NotoSans = Noto_Sans({ weight: "400", subsets: ["latin"] });
 
+export const metadata: Metadata = {
+  title: "Pixel Perfect",
+  description: "A technical blog about web development and more",
+  icons: {
+    icon: "/pixel-p-modified.svg",
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -13,6 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      <head>
+        <link rel="icon" href="/pixel-p-modified.svg" type="image/svg+xml" />
+      </head>
       <body className={NotoSans.className}>
         <div id="flex-container" className="flex min-h-screen flex-col">
           <Header />
